d545ea4d4c8713effe5fe5b130abbf3cb5aa6e16
mrr0pr0 <robert.talberg@icloud.com>
2026-01-12 08:37:27 +0100

    Add authentication functions for sign up, sign in, sign out, and get current user

---
+++ components/auth/AuthForm.jsx
@@ -0,0 +1,27 @@
+import { exportTraceState } from "next/dist/trace";
+import { supabase } from "./supabase";
+
+export async function signUp(email, password) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+  return { data, error };
+}
+
+export async function signIn(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  return { data, error };
+}
+
+export async function signOut() {
+  const { error } = await supabase.auth.signOut();
+  return { error };
+}
+
+export async function getCurrentUser(params) {
+    
+}
